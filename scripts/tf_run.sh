5cb72fb74f84c5bc8ff9318e4bf59f793612697f
Celso <celso.coutinho@nordcloud.com>
2021-12-01 10:56:22 +0100

    Read environment in the script responsible for running terraform

--- scripts/tf_run.sh
+++ scripts/tf_run.sh
@@ -1,6 +1,9 @@
 # Exit when any command returns a failure status.
 set -e
 
+# Read arguments
+environment=$1
+
 # Initialize Terraform.
 terraform init -input=false -backend-config="environments/$environment/$environment.backend.tfvars"
 
